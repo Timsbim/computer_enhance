0d3b30da1a0998eb06ceaaa1369d9c62649e0603
Timsbim <62063372+Timsbim@users.noreply.github.com>
2023-11-21 21:23:51 +0000

    Update: solution 1. part of 2. homework

---
+++ Solutions/sim8086_1.1.py
@@ -0,0 +1,45 @@
+from argparse import ArgumentParser, FileType
+from itertools import product
+
+
+# REG/RM-W field encoding (Table 4-9)
+REGRM_W_ENC = {
+    key: r + w for key, (w, r) in enumerate(product("lhx", "acdb"))
+} | {12: "sp", 13: "bp", 14: "si", 15: "di"}
+
+
+# Read command line argument: input file
+parser = ArgumentParser()
+parser.add_argument("file", type=FileType("rb"))
+args = parser.parse_args()
+
+
+print("; Listing produced by bit manipulation approach")
+print("bits 16", end="\n\n")
+
+# Processing file
+for bytes in args.file:
+    # Remove eol chars
+    bytes = bytes.rstrip()
+
+    # Split byte stream into consecutive pairs
+    for i in range(0, len(bytes), 2):
+        
+        first_byte = bytes[i]
+        OP = first_byte >> 2        # First 6 bits
+        D  = (first_byte >> 1) & 1  # 7. bit
+        W  = first_byte & 1         # 8. bit
+
+        second_byte = bytes[i + 1]
+        MOD = second_byte >> 6        # First 2 bits
+        REG = (second_byte >> 3) & 7  # Bits 3 - 5
+        RM  = second_byte & 7         # Bits 6 - 8
+
+        if OP == 34 and MOD == 3:
+            reg1 = REGRM_W_ENC[REG + W * 8]
+            reg2 = REGRM_W_ENC[RM + W * 8]
+            if D == 0:
+                reg1, reg2 = reg2, reg1
+            print(f"mov {reg1}, {reg2}")
+        else:
+            print("Instruction(s) unknown!")
--- sim8086.py
+++ sim8086.py
@@ -7,6 +7,12 @@ REGRM_W_ENC = {
     key: r + w for key, (w, r) in enumerate(product("lhx", "acdb"))
 } | {12: "sp", 13: "bp", 14: "si", 15: "di"}
 
+# R/M encoding for MOD = 00, 01, 10
+RM_ENC = {
+    0: "bx + si", 1: "bx + di", 2: "bp + si", 3: "bp + di",
+    4: "si", 5: "di", 6: "bp", 7: "bx"
+}
+
 
 # Read command line argument: input file
 parser = ArgumentParser()
@@ -17,29 +23,65 @@ args = parser.parse_args()
 print("; Listing produced by bit manipulation approach")
 print("bits 16", end="\n\n")
 
-# Processing file
-for bytes in args.file:
-    # Remove eol chars
-    bytes = bytes.rstrip()
+# Read file
+bytes = args.file.read()
 
-    # Split byte stream into consecutive pairs
-    for i in range(0, len(bytes), 2):
+# Processing data
+num_bytes = len(bytes)
+i = 0
+while i < num_bytes:
+    
+    # Process first byte of instruction
+    first_byte = bytes[i]
+    i += 1
+    if first_byte >> 2 == 34:  # Register/memory to/from register
         
-        first_byte = bytes[i]
-        OP = first_byte >> 2        # First 6 bits
         D  = (first_byte >> 1) & 1  # 7. bit
         W  = first_byte & 1         # 8. bit
-
-        second_byte = bytes[i + 1]
+        
+        second_byte = bytes[i]
+        i += 1
         MOD = second_byte >> 6        # First 2 bits
         REG = (second_byte >> 3) & 7  # Bits 3 - 5
         RM  = second_byte & 7         # Bits 6 - 8
 
-        if OP == 34 and MOD == 3:
-            reg1 = REGRM_W_ENC[REG + W * 8]
+        reg = REGRM_W_ENC[REG + W * 8]
+        if MOD < 3:  # Memory mode
+            
+            disp = 0
+            if MOD == 0:  # Memory mode, no displacement
+                if RM == 6:  # Direct address
+                    pass
+            elif MOD == 1:  # 8-bit displacement
+                disp = bytes[i]
+                i += 1
+            else:  # 16-bit displacement
+                disp = int.from_bytes(bytes[i:i + 2], byteorder="little")
+                i += 2
+            mem = f"[{RM_ENC[RM]}" + (f" + {disp}]" if disp != 0 else "]")
+            
+            if D == 0:  # From register into memory
+                print(f"mov {mem}, {reg}")
+            else:  # From memory into register
+                print(f"mov {reg}, {mem}")
+        
+        else:  # Register mode
+            
             reg2 = REGRM_W_ENC[RM + W * 8]
             if D == 0:
-                reg1, reg2 = reg2, reg1
-            print(f"mov {reg1}, {reg2}")
+                reg, reg2 = reg2, reg
+            print(f"mov {reg}, {reg2}")
+    
+    elif first_byte >> 4 == 11:  # Immediate to register
+        
+        W   = (first_byte >> 3) & 1  # 5. bit
+        if W == 0:
+            DATA = bytes[i]
+            i += 1
         else:
-            print("Instruction(s) unknown!")
+            DATA = int.from_bytes(bytes[i:i + 2], byteorder="little")
+            i += 2
+        print(f"mov {REGRM_W_ENC[(first_byte & 7) + W * 8]}, {DATA}")
+    
+    else:
+        print("Instruction(s) unknown!")
