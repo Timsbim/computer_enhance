c0d4a211fe268f5b5b70b03febc1403086ef267c
Timsbim <62063372+Timsbim@users.noreply.github.com>
2023-11-23 10:53:16 +0000

    Update: 2. part of homework 2

--- sim8086.py
+++ sim8086.py
@@ -34,7 +34,45 @@ while i < num_bytes:
     # Process first byte of instruction
     first_byte = bytes[i]
     i += 1
-    if first_byte >> 2 == 34:  # Register/memory to/from register
+    if first_byte >> 1 == 99:  # Intermediate to register/memory
+        
+        W  = first_byte & 1         # 8. bit
+    
+        second_byte = bytes[i]
+        i += 1
+        MOD = second_byte >> 6        # First 2 bits
+        RM  = second_byte & 7         # Bits 6 - 8
+
+        if MOD < 3:  # Memory mode
+            
+            if MOD == 0 and RM == 6:  # Direct address
+                disp = int.from_bytes(bytes[i:i + 2], byteorder="little")
+                i += 2
+                regmem = f"[{disp}]"
+            else:
+                disp = 0
+                if MOD == 1:  # 8-bit displacement
+                    disp = bytes[i]
+                    i += 1
+                elif MOD == 2:  # 16-bit displacement
+                    disp = int.from_bytes(bytes[i:i + 2], byteorder="little")
+                    i += 2
+                regmem = f"[{RM_ENC[RM]}" + (f" + {disp}]" if disp != 0 else "]")
+        
+        else:  # Register mode
+            regmem = REGRM_W_ENC[RM + W * 8]
+            
+        if W == 0:  # Byte data
+            data = f"byte {bytes[i]}"
+            i += 1
+        else:  # Word data
+            data = int.from_bytes(bytes[i:i + 2], byteorder="little")
+            i += 2
+            data = f"word {data}"
+        
+        print(f"mov {regmem}, {data}")
+    
+    elif first_byte >> 2 == 34:  # Register/memory to/from register
         
         D  = (first_byte >> 1) & 1  # 7. bit
         W  = first_byte & 1         # 8. bit
@@ -46,19 +84,20 @@ while i < num_bytes:
         RM  = second_byte & 7         # Bits 6 - 8
 
         reg = REGRM_W_ENC[REG + W * 8]
-        if MOD < 3:  # Memory mode
-            
-            disp = 0
-            if MOD == 0:  # Memory mode, no displacement
-                if RM == 6:  # Direct address
-                    pass
-            elif MOD == 1:  # 8-bit displacement
-                disp = bytes[i]
-                i += 1
-            else:  # 16-bit displacement
+        if MOD < 3:  # Memory mode            
+            if MOD == 0 and RM == 6:  # Direct address
                 disp = int.from_bytes(bytes[i:i + 2], byteorder="little")
                 i += 2
-            mem = f"[{RM_ENC[RM]}" + (f" + {disp}]" if disp != 0 else "]")
+                mem = f"[{disp}]"
+            else:
+                disp = 0
+                if MOD == 1:  # 8-bit displacement
+                    disp = bytes[i]
+                    i += 1
+                elif MOD == 2:  # 16-bit displacement
+                    disp = int.from_bytes(bytes[i:i + 2], byteorder="little")
+                    i += 2
+                mem = f"[{RM_ENC[RM]}" + (f" + {disp}]" if disp != 0 else "]")
             
             if D == 0:  # From register into memory
                 print(f"mov {mem}, {reg}")
@@ -70,6 +109,7 @@ while i < num_bytes:
             reg2 = REGRM_W_ENC[RM + W * 8]
             if D == 0:
                 reg, reg2 = reg2, reg
+            
             print(f"mov {reg}, {reg2}")
     
     elif first_byte >> 4 == 11:  # Immediate to register
@@ -81,6 +121,7 @@ while i < num_bytes:
         else:  # Value in 2 bytes
             DATA = int.from_bytes(bytes[i:i + 2], byteorder="little")
             i += 2
+        
         print(f"mov {REGRM_W_ENC[(first_byte & 7) + W * 8]}, {DATA}")
     
     else:
